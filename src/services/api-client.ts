09c0bee2ecaca7bcf161d8b29d9b8e5ddff6002e
kianibound <kianibound@gmail.com>
2023-04-13 16:49:54 +0330

    commit

--- src/services/api-client.ts
+++ src/services/api-client.ts
@@ -3,6 +3,8 @@ import axios from "axios";
 export default axios.create({
   baseURL: "https://api.rawg.io/api",
   params: {
-    key: "a13006eebe8b4c0d9c9eda9353ff108b",
+    key: "a13006eebe8b4c0d9c9eda9353ff108bN",
   },
 });
+
+// test for commit
